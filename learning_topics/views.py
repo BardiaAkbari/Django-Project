d775ab43e6588085bafd2d3677fcbb241c95e15a
Bardia <Bardiaa830@gmail.com>
2023-08-06 16:24:38 +0330

    add form to the project

--- learning_topics/views.py
+++ learning_topics/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Topic
+from .form import TopicFrom
 
 
 def index(request):
@@ -20,7 +21,7 @@ def topics(request):
 
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
-    template =loader.get_template('topic.html')
+    template = loader.get_template('topic.html')
     entries = topic.entry_set.order_by('-added_date')
     context = {
         'topic': topic,
@@ -28,6 +29,17 @@ def topic(request, topic_id):
     }
     return HttpResponse(template.render(context, request))
 
-def new_topic(request):
 
-    print("kon")
+def new_topic(request):
+    template = loader.get_template('new_topic.html')
+
+    if request.method != 'POST':
+        form = TopicFrom()
+    else:
+        form = TopicFrom(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('topics')
+
+    context = {'form': form}
+    return HttpResponse(template.render(context, request))
